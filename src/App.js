ced4ef22b1246d1e1775d7d39be9c69f87d1ec64
Silvia Gonzalves <sgonzal0@hotmail.com>
2022-08-11 12:40:24 -0300

    primer paso de RJYesenia

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import logo from './logoyesenia.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Mi nombre : Silvia Gonzalves Fariña</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi proyecto Yesenia -Manos que brillan
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Documentacion de  React
         </a>
       </header>
     </div>
