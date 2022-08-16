f134733f380c11cfbe7a2426b4a3e2ac306ee12b
Silvia Gonzalves <sgonzal0@hotmail.com>
2022-08-16 16:40:48 -0300

    desafio entregable 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logoyesenia.png';
 import './App.css';
+import NavBar from "./Components/NavBar.js"
+import Contenido from "./Components/Contenido.js"
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Mi nombre : Silvia Gonzalves Fariña</h3>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mi proyecto Yesenia -Manos que brillan
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Documentacion de  React
-        </a>
-      </header>
+    <div >
+
+      <NavBar/>
+      
+      <Contenido/>
     </div>
   );
 }
---
+++ src/Components/Contenido.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const Contenido = () => {
+    return (
+        <section className="container-fluid classContenido" >
+           <h1 className="tituloPpal">Lista de Productos</h1>
+           <p className="classP"> Acá muestro todos los productos</p>
+
+        </section>
+    )
+}
+export default Contenido
